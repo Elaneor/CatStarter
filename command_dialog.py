ddaa1e18368c80b39f89034d7d45cff12a7a814f
Elaneor <elaneor@gmail.com>
2026-06-16 22:03:20 +0300

    Update command_dialog.py
    Доработан запуск URL-cсылок

--- command_dialog.py
+++ command_dialog.py
@@ -1,6 +1,21 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 
+def enable_ctrl_v(widget):
+    def paste_event(event=None):
+        try:
+            widget.insert(tk.INSERT, widget.clipboard_get())
+        except tk.TclError:
+            pass
+        return "break"
+
+    def on_keypress(event):
+        if event.state & 0x4 and event.keycode == 86:
+            return paste_event(event)
+        return None
+
+    widget.bind("<KeyPress>", on_keypress, add="+")
+
 
 def open_command_dialog(master, center_window, command_data=None, on_save=None):
     dialog = tk.Toplevel(master)
@@ -23,9 +38,12 @@ def open_command_dialog(master, center_window, command_data=None, on_save=None):
     frame.pack(fill="both", expand=True)
 
     ttk.Label(frame, text="Наименование:").grid(row=0, column=0, sticky="w", pady=3)
-    ttk.Entry(frame, textvariable=name_var).grid(row=0, column=1, sticky="ew", pady=3)
-
+    entry_name = ttk.Entry(frame, textvariable=name_var)
+    entry_name.grid(row=0, column=1, sticky="ew", pady=3)
+    enable_ctrl_v(entry_name)
+    
     ttk.Label(frame, text="Тип:").grid(row=1, column=0, sticky="w", pady=3)
+    
     ttk.Combobox(
         frame,
         textvariable=type_var,
@@ -34,14 +52,21 @@ def open_command_dialog(master, center_window, command_data=None, on_save=None):
     ).grid(row=1, column=1, sticky="ew", pady=3)
 
     ttk.Label(frame, text="Команда:").grid(row=2, column=0, sticky="w", pady=3)
-    ttk.Entry(frame, textvariable=command_var).grid(row=2, column=1, sticky="ew", pady=3)
+    entry_command = ttk.Entry(frame, textvariable=command_var)
+    entry_command.grid(row=2, column=1, sticky="ew", pady=3)
+    enable_ctrl_v(entry_command)
 
     ttk.Label(frame, text="Параметры:").grid(row=3, column=0, sticky="w", pady=3)
-    ttk.Entry(frame, textvariable=parameters_var).grid(row=3, column=1, sticky="ew", pady=3)
-
+    entry_parameters = ttk.Entry(frame, textvariable=parameters_var)
+    entry_parameters.grid(row=3, column=1, sticky="ew", pady=3)
+    enable_ctrl_v(entry_parameters)
+    
+    
     ttk.Label(frame, text="Рабочий каталог:").grid(row=4, column=0, sticky="w", pady=3)
-    ttk.Entry(frame, textvariable=workdir_var).grid(row=4, column=1, sticky="ew", pady=3)
-
+    entry_workdir = ttk.Entry(frame, textvariable=workdir_var)
+    entry_workdir.grid(row=4, column=1, sticky="ew", pady=3)
+    enable_ctrl_v(entry_workdir)
+    
     frame.columnconfigure(1, weight=1)
 
     button_frame = ttk.Frame(dialog)
